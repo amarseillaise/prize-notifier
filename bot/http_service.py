60ec13d7f9592a28f30af1a1eeab987fb8057cd5
Tagro <aliev@simple-kit.ru>
2024-12-10 00:52:33 +0300

    FIXED internal api error

--- bot/http_service.py
+++ bot/http_service.py
@@ -1,8 +1,7 @@
-from venv import logger
-
 import requests
 import json
 from dataclasses import dataclass
+from logging import log
 
 from app.models import PrizeModel
 
@@ -13,23 +12,34 @@ class DobryHttpService:
 
     def get_prizes_list(self) -> list[PrizeModel]:
         http_method = 'prizes/'
-        try:
-            response = self._get(http_method)
-        except Exception:
-            logger.error('Unable to get list prizes')
+        response = self._get(http_method)
+        if not response:
             return []
         prizes = [PrizeModel(**item) for item in json.loads(response.content)]
         return prizes
 
     def _get(self, method_uri) -> requests.Response | None:
         url = f'{self._url.get_url()}/{method_uri}'
-        response = requests.get(url)
+        response = self._make_request(requests.get, url=url)
         return response
 
     def _post(self, method_uri) -> requests.Response | None:
         url = f'{self._url.get_url()}/{method_uri}'
-        response = requests.post(url)
+        response = self._make_request(requests.post, url=url)
         return response
+
+    def _make_request(self, method, **kwargs) -> requests.Response | None:
+        response = self._try_request(method(**kwargs))
+        if response and response.status_code != 200:
+            return None
+        return response
+
+    def _try_request(self, method, **kwargs) -> requests.Response | None:
+        try:
+            return method(**kwargs)
+        except Exception as e:
+            log(0, str(e), exc_info=e)
+            return None
 
     @dataclass
     class Url:
